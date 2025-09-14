cd6036813f2b7ca013053252bdd7ac72adc62c69
akshitvigg <akshitvig48@gmail.com>
2025-09-14 19:41:23 +0530

    basic backend done

---
+++ app/api/test/route.ts
@@ -0,0 +1,17 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { NextResponse } from "next/server";
+
+const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!)
+export async function POST(req: Request) {
+
+  try {
+    const { message } = await req.json();
+    const model = genAi.getGenerativeModel({ model: "gemini-1.5-flash" })
+    const result = await model.generateContent(message)
+    const text = result.response.text();
+
+    return NextResponse.json({ success: true, text: text })
+  } catch (err: any) {
+    return NextResponse.json({ success: false, error: err.message }, { status: 500 })
+  }
+}
