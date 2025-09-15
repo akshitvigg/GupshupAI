5e3dbeb77e51b41c2513aa54ed59fab44a291a65
akshitvigg <akshitvig48@gmail.com>
2025-09-15 22:36:13 +0530

    chat type input ref

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 import axios from "axios"
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 
 export default function Home() {
   const [msgs, setMsgs] = useState<{ role: string, text: string }[]>([])
   const [input, setInput] = useState("")
   const [sidebarOpen, setSidebarOpen] = useState(true)
+  const textAreaRef = useRef<HTMLTextAreaElement | null>(null)
 
   const sendMsgs = async () => {
 
@@ -19,12 +20,12 @@ export default function Home() {
   return (
     <div className="flex text-black min-h-screen ">
 
-      <div className={` ${sidebarOpen ? "w-44" : "w-14"} transition-all duration-300 ease-in-out   bg-black  text-white`}>
+      <div className={` ${sidebarOpen ? "w-44" : "w-14"} transition-all duration-300 ease-in-out   bg-[#171717] text-white`}>
         <span onClick={() => setSidebarOpen(!sidebarOpen)}>heloo</span>
       </div>
 
       {/* main chat area */}
-      <div className="bg-neutral-700 flex-row justify-between w-full">
+      <div className="bg-[#0A0A0A] flex flex-col justify-between w-full">
         <div className=" flex-1 p-4 overflow-y-auto">
           {msgs.map((m, i) => (
             <div key={i} className={`${m.role === "assistant" ? "text-green-400" : "text-white"}`}>
@@ -34,21 +35,18 @@ export default function Home() {
         </div>
 
 
-
-        <div className="flex p-4 bg-violet-950">
-          <input className="flex-1 p-2 mr-2 rounded"
-            value={input}
-            onChange={(e) => { setInput(e.target.value) }}
-            placeholder="write your prompt"
-          />
-          <button className="px-4 text-white py-2 bg-black rounded" onClick={sendMsgs}>send</button>
+        <div className=" flex justify-center">
+          <div className="flex p-4 w-[600px] items-end bg-violet-950">
+            <textarea />
+            <button className="px-4 text-white py-2 bg-black rounded" onClick={sendMsgs}>send</button>
+          </div>
         </div>
       </div>
 
 
 
 
-    </div>
+    </div >
 
   )
 }
